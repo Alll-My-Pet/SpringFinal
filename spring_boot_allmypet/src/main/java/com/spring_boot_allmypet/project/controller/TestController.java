5d870095a27ef4cb2ebefa0a62911cd46b7b690b
hoeandpick <wjswldms0217@naver.com>
2024-08-07 00:55:54 +0900

    스프링 테스트

--- spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/TestController.java
+++ spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/TestController.java
@@ -11,4 +11,24 @@ public class TestController {
 		return "index";
 	}
 	
+	@RequestMapping("/login")
+	public String login() {
+		return "member/login";
+	}
+	
+	@RequestMapping("/join")
+	public String join() {
+		return "member/join";
+	}
+	
+	@RequestMapping("/forgotID")
+	public String forgotID() {
+		return "member/forgotID";
+	}
+	
+	@RequestMapping("/forgotPW")
+	public String forgotPW() {
+		return "member/forgotPW";
+	}
+
 }
