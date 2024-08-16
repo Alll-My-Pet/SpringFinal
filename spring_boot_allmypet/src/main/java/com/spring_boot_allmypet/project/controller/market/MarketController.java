af2495fbd577912a553cc9e582a8208dd2febf03
noohJ <tlsdlwjd3@gmail.com>
2024-08-16 20:31:59 +0900

    animalMapper삭제

--- spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/market/MarketController.java
+++ spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/market/MarketController.java
@@ -7,17 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import jakarta.servlet.http.HttpSession;
 
 import com.spring_boot_allmypet.project.model.market.CartVO;
 import com.spring_boot_allmypet.project.model.market.MemberVO;
-import com.spring_boot_allmypet.project.model.market.OrderInfoVO;
 import com.spring_boot_allmypet.project.model.market.ProductVO;
 import com.spring_boot_allmypet.project.service.market.CartService;
 import com.spring_boot_allmypet.project.service.market.OrderService;
 import com.spring_boot_allmypet.project.service.market.ProductService;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
