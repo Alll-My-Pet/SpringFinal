5424b6647312d677c708e95c9be7737cc6301385
catchimmy20 <kyuri20@sookmyung.ac.kr>
2024-08-16 15:52:44 +0900

    확인용 커밋

--- spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/animal/AnimalController.java
+++ spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/controller/animal/AnimalController.java
@@ -1,7 +1,9 @@
 package com.spring_boot_allmypet.project.controller.animal;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class AnimalController {
 	// 동물별 커뮤니티 메인 페이지 열기
 	@RequestMapping("/animal_home")
