6457fd0538a812a366edfd717dfba5258dae7eb3
KCU <hanhee1113@naver.com>
2024-08-28 15:39:38 +0900

    20240828

---
+++ spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/service/map/PetMapService.java
@@ -0,0 +1,34 @@
+package com.spring_boot_allmypet.project.service.map;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.spring_boot_allmypet.project.dao.map.IPetMapDAO;
+import com.spring_boot_allmypet.project.model.map.PetMapVO;
+
+@Service
+public class PetMapService {
+	@Autowired
+	@Qualifier("IPetMapDAO")
+	IPetMapDAO dao;
+	
+	public ArrayList<PetMapVO> getAllStationList() {
+		return dao.getAllStationList();
+	}
+	
+
+
+	public ArrayList<PetMapVO> getStationsByCity(String cityName) {
+		return dao.getStationsByCity();
+	}
+
+
+
+    public ArrayList<PetMapVO> getStationsByBounds(Map<String, Object> bounds) {
+        return dao.getStationsByBounds(bounds);
+    }
+}
