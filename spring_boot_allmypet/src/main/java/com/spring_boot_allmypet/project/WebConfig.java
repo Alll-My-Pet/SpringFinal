2d9fd90314c28eeef101cadb892b9ded5c1ff62f
noohJ <tlsdlwjd3@gmail.com>
2024-09-12 09:59:28 +0900

    test

--- spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/WebConfig.java
+++ spring_boot_allmypet/src/main/java/com/spring_boot_allmypet/project/WebConfig.java
@@ -9,11 +9,17 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-		 // 현재 작업 중인 프로젝트의 루트 경로
-		 String projectRootPath = System.getProperty("user.dir");
+		// 현재 작업 중인 프로젝트의 루트 경로
+		String projectRootPath = System.getProperty("user.dir");
 		// 맵핑 이름 : project_images
 		registry.addResourceHandler("/project_images/**")
 		.addResourceLocations("file:///" + projectRootPath + "/src/main/webapp/uploads/");
+		
+		registry.addResourceHandler("/prd_images/**")
+		.addResourceLocations("file:///usr/local/allmypet/product_images/");
+		
+		registry.addResourceHandler("/emoji_imgs/**")
+		.addResourceLocations("file:///usr/local/allmypet/emoji_imgs/");
 
 	}
 }
